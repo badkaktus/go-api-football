ba7bf8a055f831e4fec7fc823d6a06e3b23fd5b8
Nikolai Siniukhin <nikolai.siniukhin@agcsoft.com>
2022-08-25 17:29:31 +0300

    start to split struct

--- helpers.go
+++ helpers.go
@@ -10,3 +10,38 @@ type successResponse struct {
 	} `json:"paging"`
 	Response interface{} `json:"response"`
 }
+
+type TeamExtendedInfo struct {
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Code     string `json:"code"`
+	Country  string `json:"country"`
+	Founded  int    `json:"founded"`
+	National bool   `json:"national"`
+	Logo     string `json:"logo"`
+}
+
+type Venue struct {
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Address  string `json:"address"`
+	City     string `json:"city"`
+	Capacity int    `json:"capacity"`
+	Surface  string `json:"surface"`
+	Image    string `json:"image"`
+}
+
+type League struct {
+	ID      int    `json:"id"`
+	Name    string `json:"name"`
+	Country string `json:"country"`
+	Logo    string `json:"logo"`
+	Flag    string `json:"flag"`
+	Season  int    `json:"season"`
+}
+
+type TeamShortInfo struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	Logo string `json:"logo"`
+}
--- teams.go
+++ teams.go
@@ -8,26 +8,9 @@ import (
 	"github.com/google/go-querystring/query"
 )
 
-// todo team and venue to separate struct
 type Teams []struct {
-	Team struct {
-		ID       int    `json:"id"`
-		Name     string `json:"name"`
-		Code     string `json:"code"`
-		Country  string `json:"country"`
-		Founded  int    `json:"founded"`
-		National bool   `json:"national"`
-		Logo     string `json:"logo"`
-	} `json:"team"`
-	Venue struct {
-		ID       int    `json:"id"`
-		Name     string `json:"name"`
-		Address  string `json:"address"`
-		City     string `json:"city"`
-		Capacity int    `json:"capacity"`
-		Surface  string `json:"surface"`
-		Image    string `json:"image"`
-	} `json:"venue"`
+	Team  TeamExtendedInfo `json:"team"`
+	Venue Venue            `json:"venue"`
 }
 
 type TeamsOptions struct {
@@ -104,8 +87,8 @@ type TeamStatistics struct {
 			} `json:"average"`
 			Minute struct {
 				Zero15 struct {
-					Total      int    `json:"total"`
-					Percentage string `json:"percentage"`
+					Total      interface{} `json:"total"`
+					Percentage interface{} `json:"percentage"`
 				} `json:"0-15"`
 				One630 struct {
 					Total      int    `json:"total"`
@@ -120,8 +103,8 @@ type TeamStatistics struct {
 					Percentage string `json:"percentage"`
 				} `json:"46-60"`
 				Six175 struct {
-					Total      int    `json:"total"`
-					Percentage string `json:"percentage"`
+					Total      interface{} `json:"total"`
+					Percentage interface{} `json:"percentage"`
 				} `json:"61-75"`
 				Seven690 struct {
 					Total      int    `json:"total"`
@@ -154,16 +137,16 @@ type TeamStatistics struct {
 					Percentage string `json:"percentage"`
 				} `json:"0-15"`
 				One630 struct {
-					Total      int    `json:"total"`
-					Percentage string `json:"percentage"`
+					Total      interface{} `json:"total"`
+					Percentage interface{} `json:"percentage"`
 				} `json:"16-30"`
 				Three145 struct {
-					Total      int    `json:"total"`
-					Percentage string `json:"percentage"`
+					Total      interface{} `json:"total"`
+					Percentage interface{} `json:"percentage"`
 				} `json:"31-45"`
 				Four660 struct {
-					Total      int    `json:"total"`
-					Percentage string `json:"percentage"`
+					Total      interface{} `json:"total"`
+					Percentage interface{} `json:"percentage"`
 				} `json:"46-60"`
 				Six175 struct {
 					Total      int    `json:"total"`
@@ -191,12 +174,12 @@ type TeamStatistics struct {
 			Loses int `json:"loses"`
 		} `json:"streak"`
 		Wins struct {
-			Home string `json:"home"`
-			Away string `json:"away"`
+			Home interface{} `json:"home"`
+			Away string      `json:"away"`
 		} `json:"wins"`
 		Loses struct {
-			Home string `json:"home"`
-			Away string `json:"away"`
+			Home interface{} `json:"home"`
+			Away string      `json:"away"`
 		} `json:"loses"`
 		Goals struct {
 			For struct {
@@ -261,13 +244,17 @@ type TeamStatistics struct {
 				Percentage string `json:"percentage"`
 			} `json:"76-90"`
 			Nine1105 struct {
-				Total      interface{} `json:"total"`
-				Percentage interface{} `json:"percentage"`
+				Total      int    `json:"total"`
+				Percentage string `json:"percentage"`
 			} `json:"91-105"`
 			One06120 struct {
 				Total      interface{} `json:"total"`
 				Percentage interface{} `json:"percentage"`
 			} `json:"106-120"`
+			NAMING_FAILED struct {
+				Total      int    `json:"total"`
+				Percentage string `json:"percentage"`
+			} `json:""`
 		} `json:"yellow"`
 		Red struct {
 			Zero15 struct {
@@ -279,20 +266,20 @@ type TeamStatistics struct {
 				Percentage interface{} `json:"percentage"`
 			} `json:"16-30"`
 			Three145 struct {
-				Total      interface{} `json:"total"`
-				Percentage interface{} `json:"percentage"`
+				Total      int    `json:"total"`
+				Percentage string `json:"percentage"`
 			} `json:"31-45"`
 			Four660 struct {
 				Total      interface{} `json:"total"`
 				Percentage interface{} `json:"percentage"`
 			} `json:"46-60"`
 			Six175 struct {
-				Total      interface{} `json:"total"`
-				Percentage interface{} `json:"percentage"`
+				Total      int    `json:"total"`
+				Percentage string `json:"percentage"`
 			} `json:"61-75"`
 			Seven690 struct {
-				Total      interface{} `json:"total"`
-				Percentage interface{} `json:"percentage"`
+				Total      int    `json:"total"`
+				Percentage string `json:"percentage"`
 			} `json:"76-90"`
 			Nine1105 struct {
 				Total      interface{} `json:"total"`
